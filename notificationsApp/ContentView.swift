9f52f476b7f88f28626916d11ac5d22bdcbb0685
Steven barnett <steve@sabarnett.co.uk>
2024-07-15 13:43:38 +0100

    Initial Commit

---
+++ notificationsApp/ContentView.swift
@@ -0,0 +1,29 @@
+//
+// -----------------------------------------
+// Original project: notificationsApp
+// Original package: notificationsApp
+// Created on: 15/07/2024 by: Steven Barnett
+// Web: http://www.sabarnett.co.uk
+// GitHub: https://www.github.com/sabarnett
+// -----------------------------------------
+// Copyright © 2024 Steven Barnett. All rights reserved.
+//
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ notificationsApp/notificationsAppApp.swift
@@ -0,0 +1,22 @@
+//
+// -----------------------------------------
+// Original project: notificationsApp
+// Original package: notificationsApp
+// Created on: 15/07/2024 by: Steven Barnett
+// Web: http://www.sabarnett.co.uk
+// GitHub: https://www.github.com/sabarnett
+// -----------------------------------------
+// Copyright © 2024 Steven Barnett. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct notificationsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
